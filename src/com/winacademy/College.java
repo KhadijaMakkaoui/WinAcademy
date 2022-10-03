44096bfd438bad6e318b5f1a3e368c0e755c4e66
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-10-03 06:07:55 +0100

    create departement attributs create constructer and getters and setters

--- src/com/winacademy/College.java
+++ src/com/winacademy/College.java
@@ -1,5 +1,37 @@
 package com.winacademy;
 
 public class College {
+    public int getCollege_id() {
+        return college_id;
+    }
 
+    public void setCollege_id(int college_id) {
+        this.college_id = college_id;
+    }
+
+    public int getCollege_Name() {
+        return college_Name;
+    }
+
+    public void setCollege_Name(int college_Name) {
+        this.college_Name = college_Name;
+    }
+
+    public String getSiteInternet() {
+        return siteInternet;
+    }
+
+    public void setSiteInternet(String siteInternet) {
+        this.siteInternet = siteInternet;
+    }
+
+    private int college_id;
+    private int college_Name;
+    private String siteInternet;
+
+    public College(int college_id, int college_Name, String siteInternet) {
+        this.college_id = college_id;
+        this.college_Name = college_Name;
+        this.siteInternet = siteInternet;
+    }
 }
