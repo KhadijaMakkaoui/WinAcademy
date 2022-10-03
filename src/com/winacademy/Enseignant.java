40d68a59ee638cf5276bd1f50a3aad8574c313cd
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-10-03 11:56:55 +0100

    Add Enseignant attributes

--- src/com/winacademy/Enseignant.java
+++ src/com/winacademy/Enseignant.java
@@ -1,6 +1,13 @@
 package com.winacademy;
+
+import java.util.Date;
+
 public class Enseignant extends User {
-    public Enseignant(int id, String nom, String prenom, String tel, String mail, String password, String role) {
+    private Date dateFonction;
+    private String matiere;
+    public Enseignant(int id, String nom, String prenom, String tel, String mail, String password, String role, Date dateFonction, String matiere) {
         super(id, nom, prenom, tel, mail, password, role);
+        this.dateFonction = dateFonction;
+        this.matiere = matiere;
     }
 }
